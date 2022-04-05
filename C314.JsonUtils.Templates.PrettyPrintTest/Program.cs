3172c6342a412da93c22aadc31dd4037bfdcffbf
314ShadePi <erikkirkegaardstage@gmail.com>
2022-04-05 17:27:04 +0200

    PrettyPrint and ToString

---
+++ C314.JsonUtils.Templates.PrettyPrintTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using C314.JsonUtils.Templates;
+
+CList list = new CList
+{
+    name = "list",
+    boards = new List<Board>
+    {
+        new Board
+        {
+            name = "board1",
+            description = "board1 description",
+            items = new List<Item>
+            {
+                new Item
+                {
+                    name = "item1",
+                    description = "item1 description"
+                },
+                new Item
+                {
+                    name = "item2",
+                    description = "item2 description"
+                }
+            }
+        },
+        new Board
+        {
+            name = "board2",
+            description = "board2 description",
+            items = new List<Item>
+            {
+                new Item
+                {
+                    name = "item2",
+                    description = "item2 description"
+                },
+                new Item
+                {
+                    name = "item2",
+                    description = "item2 description"
+                }
+            }
+        }        
+    },
+};
+
+list.PrettyPrint();
+/*prettyprint
+ *
+ * └───name
+ *     ├───board1 : desc
+ *     |   ├───item1
+ *     |   |   └───desc
+ *     |   └───item2
+ *     |       └───desc
+ *     └───board2 : desc
+ *         ├───item1
+ *         |   └───desc
+ *         └───item2
+ *             └───desc
+ */
+Console.ReadKey();
--- C314.JsonUtils.Templates/Item.cs
+++ C314.JsonUtils.Templates/Item.cs
@@ -7,12 +7,18 @@
 
         public override string ToString()
         {
-            return $"{name}\n└───{description}";
+            return $"{name}: {description}";
+        }
+
+        public void PrettyPrint(string indent = "", bool isLast = true)
+        {
+            var marker = isLast ? "└──" : "├──";
+            var descIndent = isLast ? "   └──" : "|  └──";
+            Console.Write(indent);
+            Console.Write(marker);
+            Console.Write(name + "\n" + indent + descIndent + description);
+            Console.WriteLine();
+            indent += isLast ? "   " : "|  ";
         }
     }
 }
-
-// tree
-//"├───"
-//"└───"
-//"│   "
